1d5409a93e42e89569f09b4444ac15e94567f2c4
Ivan <IZhemerev@gmail.com>
2023-08-07 12:36:43 +0300

    Task 2 done, task 1 corrected

--- Homework1/Task2/Program.cs
+++ Homework1/Task2/Program.cs
@@ -1,9 +1,13 @@
 ﻿int a = 2;
 int b = 4;
+int c = 3;
 int max = a;
 
 if (max < b) {
     max = b;
+}
+if (max < c) {
+    max = c;
 }
 Console.WriteLine("Большее число: ");
 Console.WriteLine(max);
