a1556a4202c98fa523a411d343a6678272306d14
Ivan <IZhemerev@gmail.com>
2023-08-11 11:37:33 +0300

    Task 10 complete

---
+++ Homework2/Task10/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите трёхзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(findSecondDigit(number));
+
+int findSecondDigit(int number){
+int counter = 0;
+int secondDigit = 0;
+while (counter < 2){
+    int digit = number % 10;
+    if(counter == 1) {
+        secondDigit = digit;
+    }
+    number /= 10;
+    counter++;
+}
+return secondDigit;
+}
+
+
