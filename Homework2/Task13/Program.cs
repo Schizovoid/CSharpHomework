b3c96e47e1c247d00e2f5e0b72aa92303be8f24e
Ivan <IZhemerev@gmail.com>
2023-08-11 12:01:56 +0300

    Task 13 complete

---
+++ Homework2/Task13/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите трёхзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (findThirdDigit(number) == -1){
+    Console.WriteLine("В числе отсутствует третья цифра.");
+} else {
+    Console.WriteLine(findThirdDigit(number));
+}
+
+int findThirdDigit(int number){
+int counter = 0;
+int ThirdDigit = -1;
+while (counter < 3){
+    int digit = number % 10;
+    if(counter == 0) {
+        ThirdDigit = digit;
+    }
+    if(counter == 2){
+        if (number < 1){
+            ThirdDigit = -1;
+            break;
+        }
+    }
+    number /= 10;
+    counter++;
+}
+return ThirdDigit;
+}
