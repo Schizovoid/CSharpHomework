186e1c2236739571a71d20340baa382f6a5a4a18
Ivan <IZhemerev@gmail.com>
2023-08-11 12:07:17 +0300

    Task 15 complete

---
+++ Homework2/Task15/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (isAHoliday (number) == true) {
+    Console.WriteLine("Введённое число - выходной.");
+}
+
+if (isAHoliday (number) == false) {
+    Console.WriteLine("Введённое число - не выходной.");
+}
+
+bool isAHoliday (int num) {
+    if (num == 6 || num == 7) {
+        return true;
+    } else {
+        return false;
+    }
+}
