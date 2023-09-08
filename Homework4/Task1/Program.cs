2ea5808a294689ced9410275e5b77b8f74529943
Ivan <IZhemerev@gmail.com>
2023-09-08 09:01:32 +0300

    "fitst task done"

---
+++ Homework4/Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите первое число ");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int second = Convert.ToInt32(Console.ReadLine());
+int result = first;
+
+for (int count = 1; count<second; count++){
+    result = result*first;
+}
+Console.WriteLine(result);
