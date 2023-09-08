d75c33d11d67aa4ea65a2a3d44e20dcba56c330a
Ivan <IZhemerev@gmail.com>
2023-09-08 11:23:04 +0300

    Task 2 & 3 complete

---
+++ Homework4/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите целое число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+int[] arr = getArray(number);
+
+for(int i = 0; i < arr.Length; i++) {
+    result = result + arr[i];
+}
+
+
+Console.WriteLine(Convert.ToString(result));
+
+int[] getArray(int num) {
+    List<int> list = new List<int>();
+    while (num > 0) {
+        list.Add(num % 10);
+        num = num / 10;
+    }
+    list.Reverse();
+    return list.ToArray();
+};
---
+++ Homework4/Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿void FillArray(int[] arr){
+    Random random = new Random();
+
+    for (int i = 0; i < arr.Length; i++){
+        arr[i] = random.Next(1, 10);
+    }
+}
+void PrintArray (int[] arr){
+    for (int i = 0; i < arr.Length; i++){
+        Console.Write(arr[i] + " ");
+    }
+}
+
+int[] arr = new int[8];
+FillArray(arr);
+PrintArray(arr);
