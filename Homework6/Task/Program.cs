164412f50a7897a8aea97421911c3088c01dab1a
Ivan <IZhemerev@gmail.com>
2023-09-15 11:15:00 +0300

    Homework 6, Task 1 complete

---
+++ Homework6/Task/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] getNumbersArray (){
+    Console.WriteLine("Введите количество элементов");
+    int length = Convert.ToInt32(Console.ReadLine());
+    int[] arr = new int[length];
+    for (int count = 0; count < length; count++) {
+        Console.WriteLine("Введите следующий элемент");
+        arr[count] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+
+int countPosNumbers(int[] arr){
+    int count = 0;
+    for(int i = 0; i < arr.Length; i++){
+        if (arr[i] > 0) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int [] array = getNumbersArray();
+
+Console.WriteLine(countPosNumbers(array));
