110f478a937deebe1f6cc9eeaefca575fafaf8a3
Ivan <IZhemerev@gmail.com>
2023-09-19 11:13:09 +0300

    Task 1 done

---
+++ Homework7/Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿int m = new Random().Next(2,5);
+int n = new Random().Next(2,5);
+
+double[,] array = new double[m,n];
+
+void fillArray(double[,] arr){
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++){
+            arr[i,j] = new Random().Next(-10, 10);
+        }
+    }
+}
+
+void printArray(double[,] arr){
+        for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++){
+            Console.Write($"{arr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+fillArray(array);
+printArray(array);
