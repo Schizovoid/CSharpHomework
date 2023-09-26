45ca722ebad9ee474d32801926ff3a05d0bec760
Ivan <IZhemerev@gmail.com>
2023-09-26 08:30:33 +0300

    Task 1 done

---
+++ Homework9/Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите натуральное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 1) {
+    Console.WriteLine("Введённое число не является натуральным.");
+    return;
+}
+count(n);
+void count (int n) {
+    if (n == 0) return;
+    Console.Write($"{n} ");
+    count(n - 1);
+}
