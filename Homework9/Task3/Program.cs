61ee5fa0cae971a370ccc50985c41e1c2b79ef41
Ivan <IZhemerev@gmail.com>
2023-09-26 10:25:55 +0300

    Task 3 done

---
+++ Homework9/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите первое натуральное число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе натуральное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(ack(m,n));
+
+int ack (int m, int n) 
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+        return ack (m - 1, 1);
+    }
+    else
+    {
+        return ack(m - 1, ack(m, n - 1));
+    }
+}
