81f9254bd5ca11e113119583169745b8aac4337d
Ivan <IZhemerev@gmail.com>
2023-09-26 07:21:43 +0300

    Homework 8 partially done

---
+++ Homework8/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿int[,] array = new int[4,3];
+
+void fillArray(int[,] arr){
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++){
+            arr[i,j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void printArray(int[,] arr){
+        for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++){
+            Console.Write($"{arr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+fillArray(array);
+printArray(array);
